15dd6208d6a957253239ff0e2b4a0e525b6159c6
emem785 <32265358+emem785@users.noreply.github.com>
2020-08-11 22:36:52 +0800

    todo app

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -10,7 +10,7 @@ const RESOURCES = {
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/index.html": "70c5e1e131eda9f7b3ca558fb6f34c3e",
-"/main.dart.js": "e4ad1b4d490ef3115f842c5b7e09c4e7",
+"/main.dart.js": "4c648ad66358646440647351dd6a3258",
 "/manifest.json": "59d90caa4cae6fd75f808a733fa1e2d7"
 };
 
